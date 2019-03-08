b6bbdb2612a40e827a8379fd1ea953ea0e841617
AHMAD FARAZ <ahmad.faraz@xprt.com>
2019-03-08 16:44:31 +0500

    this is commit

--- app.js
+++ app.js
@@ -26,7 +26,13 @@ var cors = require("cors");
 var session = require("express-session");
 const path = require("path");
 app.use(express.static(path.join(__dirname, "HighPlumeClient-master/build")));
-app.get('*', (request, response) => { response.sendFile(path.join(__dirname, 'HighPlumeClient-master/build', 'index.html')); });
+
+app.use('*', (request, response) => { 
+  response.sendFile(path.join(__dirname, 'HighPlumeClient-master/build', 'index.html'));
+ });
+//  app.get('*', (request, response) => { 
+//   response.sendFile(path.join(__dirname, 'HighPlumeClient-master/build', 'index.html'));
+//  });
 app.use(cors());
 mongoose.Promise = global.Promise;
 mongoose
